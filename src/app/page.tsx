e0ba7f795fbd508c048829596efa52a7afca9fa0
Giovanni Miranda <giovannimiranda09@gmail.com>
2025-08-21 19:24:33 -0400

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { Carousel, CarouselItem } from "@/components/ui/carousel";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
 import { motion } from "motion/react";
 import Image from "next/image";
 import Link from "next/link";
@@ -637,9 +638,9 @@ export default function Home() {
       </section >
 
       {/* Section 7 */}
-      <section className="w-full h-auto p-4 bg-gray-200 flex flex-col items-center justify-center gap-8 py-10">
+      <section className="w-full h-auto p-4 bg-gray-200 flex flex-col md:flex-row items-center justify-center gap-8 py-10">
 
-        <div className="w-96 h-auto bg-amber-200 flex flex-col items-center justify-center gap-6">
+        <div className="w-96 h-auto flex flex-col items-center justify-center gap-6">
 
           <h1 className="text-2xl font-bold">Projetos de sucesso</h1>
 
@@ -653,45 +654,69 @@ export default function Home() {
 
         </div>
 
-        <div className="w-full max-w-xs sm:max-w-md md:max-w-lg lg:max-w-2xl xl:max-w-4xl mx-auto h-auto bg-red-300 flex items-center justify-center p-2 rounded-xl">
-          <Carousel>
+        <Carousel
+          plugins={[Autoplay({ delay: 3000, stopOnInteraction: false })]}
+          className="w-full max-w-xs sm:max-w-md md:max-w-lg lg:max-w-2xl xl:max-w-4xl mx-auto h-80 flex items-center justify-center p-2 rounded-xl"
+        >
+          <CarouselContent>
+
             <CarouselItem>
               <Image
                 src="/logo_bio.png"
                 alt="Projeto 1"
                 width={400}
                 height={300}
-                className="object-cover w-full h-48 sm:h-64 md:h-72 lg:h-80 rounded-lg"
+                className="object-contain w-full h-48 sm:h-64 md:h-72 lg:h-80 rounded-lg bg-white"
               />
             </CarouselItem>
+
             <CarouselItem>
               <Image
                 src="/logo_ibe.png"
                 alt="Projeto 2"
                 width={400}
                 height={300}
-                className="object-cover w-full h-48 sm:h-64 md:h-72 lg:h-80 rounded-lg"
+                className="object-contain w-full h-48 sm:h-64 md:h-72 lg:h-80 rounded-lg bg-white"
               />
             </CarouselItem>
+
             <CarouselItem>
               <Image
                 src="/logo_mn.png"
                 alt="Projeto 3"
                 width={400}
                 height={300}
-                className="object-cover w-full h-48 sm:h-64 md:h-72 lg:h-80 rounded-lg"
+                className="object-contain w-full h-48 sm:h-64 md:h-72 lg:h-80 rounded-lg bg-white"
               />
             </CarouselItem>
+
             <CarouselItem>
               <Image
                 src="/logo_ss.png"
                 alt="Projeto 4"
                 width={400}
                 height={300}
-                className="object-cover w-full h-48 sm:h-64 md:h-72 lg:h-80 rounded-lg"
+                className="object-contain w-full h-48 sm:h-64 md:h-72 lg:h-80 rounded-lg bg-white"
               />
             </CarouselItem>
-          </Carousel>
+
+          </CarouselContent>
+
+          <CarouselPrevious />
+          <CarouselNext />
+
+        </Carousel>
+
+        <div>
+
+          <Image
+            src="/logo_bio.png"
+            alt="Projeto 1"
+            width={400}
+            height={300}
+            className="object-contain w-full h-48 sm:h-64 md:h-72 lg:h-80 rounded-lg bg-white"
+          />
+
         </div>
 
       </section>
