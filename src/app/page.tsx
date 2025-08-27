9d5b03f52213e65f7774266c73ce293cae08ed55
Giovanni Miranda <giovannimiranda09@gmail.com>
2025-08-26 20:09:59 -0400

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -710,7 +710,7 @@ export default function Home() {
         <div>
 
           <Image
-            src="/logo_bio.png"
+            src="/mascote5.png"
             alt="Projeto 1"
             width={400}
             height={300}
@@ -742,7 +742,9 @@ export default function Home() {
           </div>
         </div>
 
-        <div></div>
+        <div className="">
+
+        </div>
 
       </section>
 
