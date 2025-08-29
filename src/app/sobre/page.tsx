a685a676603309b692f8fb88a3d5c4dd35f1189c
Giovanni Miranda <giovannimiranda09@gmail.com>
2025-08-28 20:54:40 -0400

    update

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -1,8 +1,16 @@
 
 export default function Sobre() {
     return (
-        <div className='w-screen h-screen flex items-center justify-center bg-gray-100'>
-            <h1 className='text-4xl font-bold'>Sobre</h1>
+        <div className='w-screen h-screen flex flex-col items-center justify-center bg-gray-100'>
+
+            <h1 className='text-4xl font-bold'>Sobre Nós</h1>
+
+            <p className='mt-4 text-lg text-center text-gray-600'>
+                A Primeiro Ato é uma agência de marketing digital especializada em posicionar marcas, gerar autoridade e entregar resultados reais.
+
+                Com uma abordagem estratégica, combinamos branding, tráfego pago, automações inteligentes e produção criativa para transformar negócios em referências digitais. Atuamos de forma consultiva, aliando performance e identidade visual com foco total em crescimento mensurável.
+            </p>
+
         </div>
     );
 }
